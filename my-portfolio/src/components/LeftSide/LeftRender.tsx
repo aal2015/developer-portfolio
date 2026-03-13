8ce5791bc5f7148aebc263ebc45db6af03899358
aal2015 <abhinavlugun@yahoo.com>
2026-03-13 15:03:17 +0530

    Created left and right starting components

---
+++ my-portfolio/src/components/LeftSide/LeftRender.tsx
@@ -0,0 +1,10 @@
+const LeftRender = () =>  {
+    return (
+        <div>
+            <h1>Abhinav Lugun</h1>
+            <p>Software Engineer</p>
+        </div>
+    )
+}
+
+export default LeftRender;
---
+++ my-portfolio/src/components/RightSide/RightRender.tsx
@@ -0,0 +1,10 @@
+const RightRender = () =>  {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Infor</p>
+        </div>
+    )
+}
+
+export default RightRender;
