145351773570c8e43f68ba2e6b9156ef229e1a1c
aal2015 <abhinavlugun@yahoo.com>
2026-03-21 09:35:10 +0530

    Highlisted key words in 'About Me' section

--- my-portfolio/src/components/RightSide/AboutMe.tsx
+++ my-portfolio/src/components/RightSide/AboutMe.tsx
@@ -4,17 +4,18 @@ const AboutMe = () => {
             <h2 className="text-3xl font-bold text-white">About Me</h2>
             <p className='my-3'>
                 Hi! I would first like to thank you for taking the time to visit my page. 
-                I’m Abhinav Lugun, and I enjoy problem-solving, creativity, and building things.
+                I’m Abhinav Lugun, and I enjoy <span className="font-bold text-white">problem-solving</span>, 
+                <span className="font-bold text-white"> creativity</span>, and <span className="font-bold text-white">building things</span>.
             </p>
             <p className='mb-3'>
-                Software engineering gives me a nice outlet to express these qualities. I enjoy 
-                tackling real-time software issues like rate limiting to prevent abuse and system 
-                overload, designing and implementing a clean and intuitive frontend, and implementing 
-                cool features for applications.
+                Software engineering gives me a nice outlet to express these qualities. I <span className="font-bold text-white">enjoy 
+                tackling real-time software issues</span> like rate limiting to prevent abuse and system 
+                overload, <span className="font-bold text-white">designing and implementing a clean and intuitive frontend</span>, 
+                and <span className="font-bold text-white">implementing cool features for applications</span>.
             </p>
             <p className='mb-3'>
-                Early in my career, I had the opportunity to work with a Thai startup company onsite and 
-                a Singaporean startup company remotely as a software developer. Through my professional 
+                Early in my career, I had the opportunity to work with a <span className="font-bold text-white">Thai startup company </span> 
+                onsite and  a <span className="font-bold text-white">Singaporean startup company</span> remotely as a software developer. Through my professional 
                 work, I have worked with technologies such as frontend, backend, web3, machine learning, 
                 and cloud deployment.
             </p>
