d923359688e0702b6d49738543342eae7384030b
aal2015 <abhinavlugun@yahoo.com>
2026-03-22 20:30:37 +0530

    Added about living in vairous countries in the about me section

--- my-portfolio/src/components/RightSide/AboutMe.tsx
+++ my-portfolio/src/components/RightSide/AboutMe.tsx
@@ -1,29 +1,33 @@
-import { IoGameController } from "react-icons/io5";
-import { FaPlane, FaTableTennis } from "react-icons/fa";
-
 const AboutMe = () => {
     return (
         <div className="text-blue-100">
-            <h2 className="text-3xl font-bold text-white">About Me <FaPlane /> <FaTableTennis /></h2>
+            <h2 className="text-3xl font-bold text-white">About Me</h2>
             <p className='my-3'>
-                Hi! I would first like to thank you for taking the time to visit my page. 
-                I’m Abhinav Lugun, and I enjoy <span className="font-bold text-white">problem-solving</span>, 
+                Hi! I would first like to thank you for taking the time to visit my page.
+                I’m Abhinav Lugun, and I enjoy <span className="font-bold text-white">problem-solving</span>,
                 <span className="font-bold text-white"> creativity</span>, and <span className="font-bold text-white">building things</span>.
             </p>
             <p className='mb-3'>
-                Software engineering gives me a nice outlet to express these qualities. I <span className="font-bold text-white">enjoy 
-                tackling real-time software issues</span> like rate limiting to prevent abuse and system 
-                overload, <span className="font-bold text-white">designing and implementing a clean and intuitive frontend</span>, 
+                Software engineering gives me a nice outlet to express these qualities. I <span className="font-bold text-white">enjoy
+                    tackling real-time software issues</span> like rate limiting to prevent abuse and system
+                overload, <span className="font-bold text-white">designing and implementing a clean and intuitive frontend</span>,
                 and <span className="font-bold text-white">implementing cool features for applications</span>.
             </p>
             <p className='mb-3'>
-                Early in my career, I had the opportunity to work with a <span className="font-bold text-white">Thai startup company </span> 
-                onsite and  a <span className="font-bold text-white">Singaporean startup company</span> remotely as a software developer. Through my professional 
-                work, I have worked with technologies such as frontend, backend, web3, machine learning, 
+                Early in my career, I had the opportunity to work with a <span className="font-bold text-white">Thai startup company </span>
+                onsite and  a <span className="font-bold text-white">Singaporean startup company</span> remotely as a software developer. Through my professional
+                work, I have worked with technologies such as frontend, backend, web3, machine learning,
                 and cloud deployment.
             </p>
             <p className='mb-3'>
-                I am interested in mastering my craft in building robust and scalable applications, and 
+                Coming from a <span className="font-bold text-white">diplomatic family</span>, I had the privilege of growing up in multiple countries, moving every few years.
+                I was born in Doha, Qatar <span className="fi fi-qa"></span>, and lived in France <span className="fi fi-fr"></span>, Algeria <span className="fi fi-dz"></span>, 
+                Philippines <span className="fi fi-ph"></span>, Nepal <span className="fi fi-np"></span>, Yemen <span className="fi fi-ye"></span>, Sudan <span className="fi fi-sd"></span>, 
+                Greece <span className="fi fi-gr"></span>, and Thailand <span className="fi fi-th"></span>. Having worked with different nationalities professionally, including Thai, Nepali, 
+                Indian, Greek, Singaporean, Myanmar, and Filipino, combined living in multiple countries, I am <span className="font-bold text-white">comfortable working with diverse teams</span> and <span className="font-bold text-white">adapt quickly to different environments</span>.
+            </p>
+            <p className='mb-3'>
+                I am interested in mastering my craft in building robust and scalable applications, and
                 exploring web3 and AI further.
             </p>
             <p>
